e6582f31a38821a8c7c7e3802d9fb121b51136a8
dn9z <28568855+dn9z@users.noreply.github.com>
2022-03-15 22:24:21 +0100

    finished title anim

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -1,28 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import "./Contact.scss";
-import { motion, useAnimation } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Contact = () => {
-  const control = useAnimation();
 
-  const charAnim1 = () => {
-    control.start({
+  const [isHovered_c1, setIsHovered_c1] = useState(false);
+  const [isHovered_o, setIsHovered_o] = useState(false);
+  const [isHovered_n, setIsHovered_n] = useState(false);
+  const [isHovered_t1, setIsHovered_t1] = useState(false);
+  const [isHovered_a, setIsHovered_a] = useState(false);
+  const [isHovered_c2, setIsHovered_c2] = useState(false);
+  const [isHovered_t2, setIsHovered_t2] = useState(false);
+  const [isHovered_m, setIsHovered_m] = useState(false);
+  const [isHovered_e, setIsHovered_e] = useState(false);
+
+ 
+
+  const charVariants = {
+    squeeze: {
       scaleY: [0.8, 0.5, 0.3],
+      // originY:0.5,
       transition: {
         duration: 1,
       },
-    });
-  };
-
-  const charAnim2 = () => {
-    control.start({
-      scaleY: [1, 1.2, 1],
-    });
-  };
-
-  const charVariants = {
-    squeeze: {},
-    loosen: {},
+    },
+    loosen: { scaleY: [1, 1.2, 1] },
   };
 
   return (
@@ -31,27 +33,77 @@ const Contact = () => {
       <div className="contact-main">
         <div className="title">
           <motion.p
-            onMouseOver={charAnim1}
-            onMouseLeave={charAnim2}
-            animate={control}
+            onMouseOver={() => setIsHovered_c1(true)}
+            onMouseLeave={() => setIsHovered_c1(false)}
+            animate={isHovered_c1 ? "squeeze" : "loosen"}
+            variants={charVariants}
           >
             C
           </motion.p>
           <motion.p
-            onMouseOver={charAnim1}
-            onMouseOut={charAnim2}
-            animate={control}
+            onMouseOver={() => setIsHovered_o(true)}
+            onMouseLeave={() => setIsHovered_o(false)}
+            animate={isHovered_o ? "squeeze" : "loosen"}
+            variants={charVariants}
           >
             o
           </motion.p>
-          <motion.p>n</motion.p>
-          <motion.p>t</motion.p>
-          <motion.p>a</motion.p>
-          <motion.p>c</motion.p>
-          <motion.p>t</motion.p>
-          <span></span>
-          <motion.p>m</motion.p>
-          <motion.p>e</motion.p>
+          <motion.p
+            onMouseOver={() => setIsHovered_n(true)}
+            onMouseLeave={() => setIsHovered_n(false)}
+            animate={isHovered_n ? "squeeze" : "loosen"}
+            variants={charVariants}
+          >
+            n
+          </motion.p>
+          <motion.p
+            onMouseOver={() => setIsHovered_t1(true)}
+            onMouseLeave={() => setIsHovered_t1(false)}
+            animate={isHovered_t1 ? "squeeze" : "loosen"}
+            variants={charVariants}
+          >
+            t
+          </motion.p>
+          <motion.p
+            onMouseOver={() => setIsHovered_a(true)}
+            onMouseLeave={() => setIsHovered_a(false)}
+            animate={isHovered_a ? "squeeze" : "loosen"}
+            variants={charVariants}
+          >
+            a
+          </motion.p>
+          <motion.p
+            onMouseOver={() => setIsHovered_c2(true)}
+            onMouseLeave={() => setIsHovered_c2(false)}
+            animate={isHovered_c2 ? "squeeze" : "loosen"}
+            variants={charVariants}
+          >
+            c
+          </motion.p>
+          <motion.p
+            onMouseOver={() => setIsHovered_t2(true)}
+            onMouseLeave={() => setIsHovered_t2(false)}
+            animate={isHovered_t2 ? "squeeze" : "loosen"}
+            variants={charVariants}
+          >
+            t
+          </motion.p>
+          <motion.p
+            onMouseOver={() => setIsHovered_m(true)}
+            onMouseLeave={() => setIsHovered_m(false)}
+            animate={isHovered_m ? "squeeze" : "loosen"}
+            variants={charVariants}
+          >
+            m
+          </motion.p>
+          <motion.p
+            onMouseOver={() => setIsHovered_e(true)}
+            onMouseLeave={() => setIsHovered_e(false)}
+            animate={isHovered_e ? "squeeze" : "loosen"}
+            variants={charVariants}
+          >
+            e
+          </motion.p>
         </div>
       </div>
     </>
