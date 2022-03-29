4a6a3699a87963c5c445669eb116fab359ccde07
dn9z <28568855+dn9z@users.noreply.github.com>
2022-03-29 11:24:14 +0200

    stable

--- src/components/anim/TitleAnim/TitleAnim.jsx
+++ src/components/anim/TitleAnim/TitleAnim.jsx
@@ -5,39 +5,55 @@ import CharacterAnim from "./CharacterAnim";
 
 const charVariants = {
   hidden: {
+    display: "none",
     opacity: 0,
   },
   show: {
+    display: "block",
     opacity: 1,
   },
 };
 
 const cursorVariants = {
   hidden: {
-    opacity:0
+    opacity: 0,
   },
   show: {
-    opacity:0.26,
-    transition:{
-      delay:2,duration:0.8,repeat: Infinity
-    }
+    opacity: 0.26,
+    transition: {
+      delay: 2,
+      duration: 0.8,
+      repeat: Infinity,
+    },
   },
-}
+};
+
+const barVariants = {
+  hidden: {
+    y: "100vh",
+  },
+  show: {
+    y: 0,
+    transition: {
+      duration: 2,
+    },
+  },
+};
 
 const TitleAnim = ({ titleString, className }) => {
   return (
     <motion.ul
       className={"title " + className}
-      transition={{ staggerChildren: 0.3 }}
+      transition={{ delayChildren: 3.6, staggerChildren: 0.3 }}
       initial="hidden"
       animate="show"
     >
       <motion.div
-className="title-bar"
-variants={barVariants}
-initial="hidden"
-animate="show"
-></motion.div>
+        className="title-bar"
+        variants={barVariants}
+        initial="hidden"
+        animate="show"
+      ></motion.div>
       {titleString.split("").map((ele, i) => {
         // console.log(ele)
         return (
@@ -46,11 +62,12 @@ animate="show"
           </motion.div>
         );
       })}
-      <motion.div 
+      <motion.div
+      variants={cursorVariants}
         className="cursor"
-        initial={{opacity:0}}
-        animate={{opacity:0.26}}
-        transition={{duration:0.8,repeat: Infinity}}
+        initial='hidden'
+        animate='show'
+        transition={{ duration: 0.8, repeat: Infinity }}
       ></motion.div>
     </motion.ul>
   );
