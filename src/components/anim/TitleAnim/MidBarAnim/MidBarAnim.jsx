152c43cf88e26771ac0d840b6a0570c3db96e62a
dn9z <28568855+dn9z@users.noreply.github.com>
2022-04-07 01:04:08 +0200

    improved mid bar

--- src/components/anim/TitleAnim/MidBarAnim/MidBarAnim.jsx
+++ src/components/anim/TitleAnim/MidBarAnim/MidBarAnim.jsx
@@ -1,38 +1,63 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import "./MidBarAnim.scss";
 import { MyContext } from "../../../Context/Context";
-const barVariants = {
-  hidden: {
-    y: "100vh",
-  },
-  transition: {
-    y: "30rem",
-  },
-  show: {
-    y: 0,
-    transition: {
-      duration: 2,
-    },
-  },
-  showAbout: {
-    y: 0,
-    // width:'2rem',
-    height: "63vh",
-    transition: {
-      duration: 2,
-    },
-  },
-  exit: {
-    y: "30rem",
-    // transition: {
-    //   duration: 2,
-    // },
-  },
-};
+import { useViewportScroll, useTransform } from "framer-motion";
+
 const MidBarAnim = () => {
   const { location } = useContext(MyContext);
+  const { scrollYProgress } = useViewportScroll();
+  const [hookedYPostion, setHookedYPosition] = useState(0);
 
+  const inputRange = [0, 1]
+  const barSize = [64, 105]
+  const tranformedBarSize = useTransform(scrollYProgress, inputRange, barSize)
+
+  useEffect(() => {
+    // console.log(scrollYProgress);
+    // console.log(hookedYPostion);
+    // hook into the onChange, store the current value as state.
+    scrollYProgress.onChange((v) => setHookedYPosition(v));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrollYProgress]); //make sure to re-subscriobe when scrollYProgress changes
+
+  const barVariants = {
+    hidden: {
+      y: "100vh",
+    },
+    transition: {
+      y: "30rem",
+    },
+    show: {
+      y: 0,
+      transition: {
+        duration: 2,
+      },
+    },
+    showAbout: (custom) => {
+      //custom can be removed
+      console.log(tranformedBarSize.current)
+      console.log(custom);
+      return {
+        y: 0,
+        // width:'2rem',
+        // height: "63vh",
+        // height: '60%',
+        // condition can potentially be removed
+        height:
+          location.pathname === "/" ? "64vh" : `${tranformedBarSize.current}vh`,
+        transition: {
+          duration: 2,
+        },
+      };
+    },
+    exit: {
+      y: "30rem",
+      // transition: {
+      //   duration: 2,
+      // },
+    },
+  };
   // console.log(location.pathname)
   return (
     <motion.div
@@ -41,6 +66,7 @@ const MidBarAnim = () => {
       initial={location.pathname === "/" ? "hidden" : "transition"}
       animate={location.pathname !== "/" ? "showAbout" : "show"}
       exit={location.pathname === "/" ? "hidden" : "transition"}
+      custom={hookedYPostion}
     ></motion.div>
   );
 };
--- src/routes/Work/Work.jsx
+++ src/routes/Work/Work.jsx
@@ -24,6 +24,38 @@ const Work = () => {
             <h1>Lorem ipsum</h1>
           </div>
         </Tilt>
+        <Tilt
+          className="tilt-container"
+          perspective={500}
+          glareEnable={true}
+          glareMaxOpacity={0.45}
+          glareBorderRadius="0.5rem"
+          scale={1.02}
+        >
+          <img
+            src="https://www.schulz-grafik.de/wp-content/uploads/2018/03/placeholder.png"
+            alt=""
+          />
+          <div className="layer-3d">
+            <h1>Lorem ipsum</h1>
+          </div>
+        </Tilt>
+        <Tilt
+          className="tilt-container"
+          perspective={500}
+          glareEnable={true}
+          glareMaxOpacity={0.45}
+          glareBorderRadius="0.5rem"
+          scale={1.02}
+        >
+          <img
+            src="https://www.schulz-grafik.de/wp-content/uploads/2018/03/placeholder.png"
+            alt=""
+          />
+          <div className="layer-3d">
+            <h1>Lorem ipsum</h1>
+          </div>
+        </Tilt>
       </div>
     </div>
   );
